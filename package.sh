92e39fe72f2d1f5ab2c1de7d007b2abce213bcb6
Paul Knopf <pauldotknopf@gmail.com>
2017-07-12 10:45:26 -0400

    Minor changes.

--- package.sh
+++ package.sh
@@ -7,8 +7,15 @@ RUN_PATH=`dirname "${SCRIPTSRC}" || echo .`
 rm -r $RUN_PATH/deploy/ || true
 mkdir $RUN_PATH/deploy/
 
-tar -cvpzf $RUN_PATH/deploy/iconv.debian64.tar.gz -C $RUN_PATH/iconv/debian64 .
-tar -cvpzf $RUN_PATH/deploy/iconv.win64.tar.gz -C $RUN_PATH/iconv/win64 .
+tar -cvpzf $RUN_PATH/deploy/iconv-1.15.debian64.tar.gz -C $RUN_PATH/iconv/debian64 .
+tar -cvpzf $RUN_PATH/deploy/iconv-1.15.win64.tar.gz -C $RUN_PATH/iconv/win64 .
 
-cd $RUN_PATH/iconv/debian64 && zip -r $RUN_PATH/deploy/iconv.debian64.zip .
-cd $RUN_PATH/iconv/win64 && zip -r $RUN_PATH/deploy/iconv.win64.zip .
+cd $RUN_PATH/iconv/debian64 && zip -r $RUN_PATH/deploy/iconv-1.15.debian64.zip .
+cd $RUN_PATH/iconv/win64 && zip -r $RUN_PATH/deploy/iconv-1.15.win64.zip .
+
+tar -cvpzf $RUN_PATH/deploy/openssl-1.0.2k.debian64.tar.gz -C $RUN_PATH/openssl/debian64 .
+tar -cvpzf $RUN_PATH/deploy/openssl-1.0.2k.win64.tar.gz -C $RUN_PATH/openssl/win64 .
+
+cd $RUN_PATH/openssl/debian64 && zip -r $RUN_PATH/deploy/openssl-1.0.2k.debian64.zip .
+cd $RUN_PATH/openssl/win64 && zip -r $RUN_PATH/deploy/openssl-1.0.2k.win64.zip .
+
