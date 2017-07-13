60d8993e67a00ef26f35af7dfca1614ca84354ac
Paul Knopf <pauldotknopf@gmail.com>
2017-07-13 10:25:47 -0400

    Oops

--- build-png.sh
+++ build-png.sh
@@ -7,4 +7,4 @@ RUN_PATH=`dirname "${SCRIPTSRC}" || echo .`
 rm -r $RUN_PATH/png/* || true
 
 docker run -it --rm -v $RUN_PATH:/work -w /work dcmtk-support-build sh scripts/build-png-win64.sh
-#docker run -it --rm -v $RUN_PATH:/work -w /work dcmtk-support-build sh scripts/build-png-debian64.sh
+docker run -it --rm -v $RUN_PATH:/work -w /work dcmtk-support-build sh scripts/build-png-debian64.sh
--- build-xml.sh
+++ build-xml.sh
@@ -7,4 +7,4 @@ RUN_PATH=`dirname "${SCRIPTSRC}" || echo .`
 rm -r $RUN_PATH/xml/* || true
 
 docker run -it --rm -v $RUN_PATH:/work -w /work dcmtk-support-build sh scripts/build-xml-win64.sh
-#docker run -it --rm -v $RUN_PATH:/work -w /work dcmtk-support-build sh scripts/build-xml-debian64.sh
+docker run -it --rm -v $RUN_PATH:/work -w /work dcmtk-support-build sh scripts/build-xml-debian64.sh
